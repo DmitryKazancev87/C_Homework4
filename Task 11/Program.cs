5d3cb8fce19b538d803b2867954f46d4b92fe72e
Dmitry Kazancev <kaz.freeman@bk.ru>
2023-04-22 22:37:42 +0300

    Черновик решения задачи 11

---
+++ Task 11/Program.cs
@@ -0,0 +1,38 @@
+﻿// 11. цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+int EnterInfomation(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+int NumberA = EnterInfomation(" Введите число A : ");
+int NumberB = EnterInfomation(" Введите число B : ");
+
+// Функция ввода в степень
+
+void Degree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1, i < = b, i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+
+Degree(NumberA, NumberB);
+
+// Ввод через фунцию EnterInformation:
+
+//int EnterInfomation (string message)
+//{
+//    Console.Write (message);
+//    return int.Parse (Console.ReadLine());
+//}
+//int Number = EnterInfomation(" Введите число N : ");
+//for (int i = 1; i <= Number; i++)
+//{
+//       Console.Write($"{i*i*i}  ");}
+
+
