452d8155f0ba2e9aa6d890c474320b1f85f01c1d
Dmitry Kazancev <kaz.freeman@bk.ru>
2023-04-23 00:40:56 +0300

    Решение задачи 13

---
+++ Task 13/Program.cs
@@ -0,0 +1,28 @@
+﻿// 13. Программа, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+
+int [] RandomArray=new int [8];
+for (int i=0;i<8;i++)
+{
+    RandomArray[i]=new Random().Next(0,9);
+    System.Console.Write(RandomArray[i]+" ");
+}
+
+
+
+//int EnterInfomation(string message)
+//{
+//    Console.Write(message);
+//    return int.Parse(Console.ReadLine()!);
+//}
+//int LengthArray = EnterInfomation(" Введите длину массива : ");
+//int [] RandomArray=new int [LengthArray];
+//for (int i=0;i<RandomArray.Length;i++)
+//{
+//    RandomArray[i]=new Random().Next(1,9);
+//    System.Console.Write(RandomArray[i]+" ");
+//}
+
+
+
