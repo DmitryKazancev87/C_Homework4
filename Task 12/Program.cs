ab0db40e1833a5c328d42db6a3f36c2c6b159162
Dmitry Kazancev <kaz.freeman@bk.ru>
2023-04-22 23:10:13 +0300

    Черновик задачи 12

---
+++ Task 12/Program.cs
@@ -0,0 +1,47 @@
+﻿// 12. Программа, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int EnterInfomation(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+int Number = EnterInfomation(" Введите число : ");
+int Length = NumberLength(Number);
+SumNumbers(Number,Length);
+
+// Функция нахождения количества цифр в числе:
+int NumberLength (int a)
+{
+    int index = 0;
+    while (a>0)
+    {
+    a=a/10;
+    index++;
+    }
+    return index;
+}
+    
+// Функция вывода сумм цифр в числе:
+
+void SumNumbers (int N, int Length)
+{
+int sum=0;
+for (int i=1; i<=Length;i++)
+{
+sum=sum%10;
+N=N/10;
+}
+System.Console.WriteLine(sum);
+}
+
+// Ввод через фунцию EnterInformation:
+
+//int EnterInfomation (string message)
+//{
+//    Console.Write (message);
+//    return int.Parse (Console.ReadLine());
+//}
+//int Number = EnterInfomation(" Введите число N : ");
+//for (int i = 1; i <= Number; i++)
+//{
+//       Console.Write($"{i*i*i}  ");}
